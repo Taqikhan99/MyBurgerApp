06006c3df0328f9849120535ea8965e3658af3f4
TaqiKhan <68019464+Taqikhan99@users.noreply.github.com>
2021-05-23 22:37:54 +0500

    navbar added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import styles from './navbar.module.css'
+import burgerlogo from '../assets/burgerlogo.png'
 class Navbar extends Component {
     render() {
         return (
             <div className={styles.navbar}>
-                <div className="brgrlogo">
-
+                <div className={styles.brgrlogo}>
+                    <img src={burgerlogo} alt="" />
+                    <h3>TK<span id={styles.lg}>Burgers</span></h3>
                 </div>
-                <div className="loginSection">
-                    
+                <div className={styles.loginSection}>
+                    <a href="#"><h3>Login</h3></a>
                 </div>
             </div>
         );
